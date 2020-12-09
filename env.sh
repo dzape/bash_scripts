90940f2b969237644f666b48f633778e98c8f80a
dzape <p.nikolic893@outlook.com>
2020-12-09 08:10:19 +0100

    Bash Basic -0.0

---
+++ env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear 
+
+echo ""
+echo " This scr will give us envi info " 
+echo " =============================== "
+
+echo "" 
+echo " Helo User - $USER"
+echo " =============================== " 
+
+echo " Your home dir is : $HOME"
+echo " =============================== " 
+
+echo " Terminal Session Type is : $TERM " 
+echo " =============================== "
